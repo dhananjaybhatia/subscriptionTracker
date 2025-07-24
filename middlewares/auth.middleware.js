2f262b269536829c0c4b877f9c0c5c564a8fa67a
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-07-24 11:28:47 +1000

    auth Middleware is implemented and tested

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+//Basically the purpse is - someone is making a request - middleware make sure before the request is completed - there is a check in between. if everything supplied by the user is correct then move to the next step.
+
+//steps are - someone is making a request getUser details -> authorize middle -> verify -> next -> get details.
+
+ const authorize = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Missing or invalid token",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password"); // Get user
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Unauthorized: User not found" });
+    }
+
+    req.user = user; // ✅ Attach user to request.
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: Invalid or expired token",
+    });
+  }
+};
+
+export default authorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getSingleUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getSingleUser);
+userRouter.get("/:id", authorize, getSingleUser);
 
 export default userRouter;
